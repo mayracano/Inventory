ceeba9368f6a660ee0801bd3b439557bf45e42b4
Mayra Cano <mayra.cano@wizeline.com>
2024-10-28 10:49:38 -0600

    Correcciones

--- src/main/java/com/example/library/service/BookInventoryService.java
+++ src/main/java/com/example/library/service/BookInventoryService.java
@@ -5,6 +5,8 @@ import com.example.library.dto.BookInventoryResponseDTO;
 
 public interface BookInventoryService {
 
+    void removeFromInventoryEvent(String event) throws Exception;
+
     BookInventoryResponseDTO addToInventory(BookInventoryDTO bookInventoryDTO);
 
     BookInventoryResponseDTO removeFromInventory(BookInventoryDTO bookInventoryDTO);
--- src/main/java/com/example/library/service/BookInventoryServiceImpl.java
+++ src/main/java/com/example/library/service/BookInventoryServiceImpl.java
@@ -6,16 +6,57 @@ import java.util.Optional;
 import com.example.library.dto.BookInventoryDTO;
 import com.example.library.dto.BookInventoryResponseDTO;
 import com.example.library.dto.BookInventoryStatus;
+import com.example.library.dto.BookReservationDTO;
+import com.example.library.dto.BookReservationEvent;
+import com.example.library.dto.BookReservationStatus;
 import com.example.library.model.BookInventory;
 import com.example.library.repository.BookInventoryRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookInventoryServiceImpl implements BookInventoryService{
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookInventoryServiceImpl.class);
+
     @Autowired
     private BookInventoryRepository bookInventoryRepository;
+
+    @Autowired
+    KafkaTemplate<String, BookReservationEvent> kafkaTemplate;
+
+    @KafkaListener(topics = "removed-inventory", groupId = "reservations-group")
+    public void removeFromInventoryEvent(String event) throws Exception {
+
+        BookReservationEvent bookReservationEvent = new ObjectMapper().readValue(event, BookReservationEvent.class);
+        LOGGER.info(String.format("Received 'removed-inventory', operation to remove a book from the inventory for user: %s and book: %s", bookReservationEvent.getBookReservation().getBookId(), bookReservationEvent.getBookReservation().getUserId()));
+
+        BookReservationDTO bookReservationDTO = bookReservationEvent.getBookReservation();
+        BookInventoryDTO bookInventoryDTO = new BookInventoryDTO();
+        bookInventoryDTO.setReservationID(bookReservationDTO.getId());
+        bookInventoryDTO.setBookId(bookReservationDTO.getBookId());
+        bookInventoryDTO.setUserId(bookReservationDTO.getUserId());
+
+        try {
+            removeFromInventory(bookInventoryDTO);
+            BookReservationEvent bookReservationCompleteEvent = new BookReservationEvent();
+            bookReservationCompleteEvent.setBookReservation(bookReservationDTO);
+            bookReservationCompleteEvent.setBookReservationStatus(BookReservationStatus.COMPLETED);
+            kafkaTemplate.send("completed-inventory", bookReservationEvent);
+            LOGGER.info(String.format("Sent 'completed-inventory' for user: %s and book: %s", bookReservationCompleteEvent.getBookReservation().getBookId(), bookReservationCompleteEvent.getBookReservation().getUserId()));
+        } catch(Exception e) {
+            BookReservationEvent bookReservationReverseEvent = new BookReservationEvent();
+            bookReservationReverseEvent.setBookReservation(bookReservationDTO);
+            bookReservationReverseEvent.setBookReservationStatus(BookReservationStatus.REVERSED);
+            kafkaTemplate.send("removed-inventory-failed", bookReservationEvent);
+            LOGGER.info(String.format("Sent 'removed-inventory-failed' for user: %s and book: %s", bookReservationReverseEvent.getBookReservation().getBookId(), bookReservationReverseEvent.getBookReservation().getUserId()));
+        }
+    }
 
     @Override
     public BookInventoryResponseDTO addToInventory(BookInventoryDTO bookInventoryDTO) {
